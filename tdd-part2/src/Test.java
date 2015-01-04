6b674128dab370b61de62c758472740728f1a2a1
Ishwar Pohani <ishwar_dskd@yahoo.com>
2015-01-04 13:03:43 +0500

    // GIT Test

---
+++ tdd-part2/src/Test.java
@@ -0,0 +1,17 @@
+
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+
+public class Test {
+	
+	@org.junit.Test
+	public void testEquality() {
+		assertTrue(true);
+	}
+
+	
+}
+
